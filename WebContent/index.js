5fb531807ad6da52258132cd11cdda7fad755bd9
Halima <halimasanofer@gmail.com>
2020-07-25 00:23:19 +0530

    added functionalities to move the ghosts

--- WebContent/index.js
+++ WebContent/index.js
@@ -122,3 +122,57 @@ function pacDotEaten(){
 		squares[pacmanCurrentIndex].classList.remove('pac-dot')
 	}
 }
+
+class Ghost {
+    constructor(className, startIndex, speed) {
+        this.className = className
+        this.startIndex = startIndex
+        this.speed = speed
+        this.currentIndex = startIndex
+        this.isScared = false
+        this.timerId = NaN
+    }
+}
+
+const ghosts = [
+    new Ghost('blinky', 348, 250),
+    new Ghost('pinky', 376, 400),
+    new Ghost('inky', 351, 300),
+    new Ghost('clyde', 379, 500)
+]
+
+//draw my ghosts onto my grid
+ghosts.forEach(ghost => {
+    squares[ghost.currentIndex].classList.add(ghost.className)
+    squares[ghost.currentIndex].classList.add('ghost')
+})
+
+//move the ghosts
+ghosts.forEach(ghost => moveGhost(ghost))
+
+function moveGhost(ghost) {
+    console.log('moved ghost')
+    const directions = [-1, +1, -width, +width]
+    let direction = directions[Math.floor(Math.random() * directions.length)]
+    console.log(direction)
+    
+    ghost.timerId = setInterval(function() {
+        //all our code
+        //if the next square does NOT contain a wall and does not contain a ghost
+        if (
+            !squares[ghost.currentIndex + direction].classList.contains('wall') &&
+            !squares[ghost.currentIndex + direction].classList.contains('ghost')
+        ) {
+                //remove any ghost
+        squares[ghost.currentIndex].classList.remove(ghost.className)
+        squares[ghost.currentIndex].classList.remove('ghost')
+        // //add direction to current Index
+        ghost.currentIndex += direction
+        // //add ghost class
+        squares[ghost.currentIndex].classList.add(ghost.className)  
+        squares[ghost.currentIndex].classList.add('ghost')  
+        } else direction = directions[Math.floor(Math.random() * directions.length)]
+
+    }, ghost.speed )
+    
+}
